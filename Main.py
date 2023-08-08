50c6c2f7b162e6482f16b814159d8ee259b65dfc
3RAS_Ryan <ryanchualeen@gmail.com>
2023-08-08 17:01:49 +0800

    testing 1

--- Main.py
+++ Main.py
@@ -18,8 +18,5 @@ def main():
         with file_path.open(mode = "w") as file:
             file.write(f'{overheads.overhead()}\n{cash_on_hand.cash_calculation()}\n{profit_loss.profit_calculation()}')
 main()
-
-
-
-
-    
+#test
+#test2
