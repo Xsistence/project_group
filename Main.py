aca75e6aae938eea8fd960c49b2c9820e8f8e286
CJYSean <chuajieyunsean@gmail.com>
2023-08-08 17:32:18 +0800

    Hi guys placed a comment SIUUUUUUUU

--- Main.py
+++ Main.py
@@ -18,5 +18,3 @@ def main():
         with file_path.open(mode = "w") as file:
             file.write(f'{overheads.overhead()}\n{cash_on_hand.cash_calculation()}\n{profit_loss.profit_calculation()}')
 main()
-
-#test final
